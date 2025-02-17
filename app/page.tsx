3db540ed196595f2a7e57d86dadd9a68079c8023
CheeseCast <jordan.nickless@ou.ac.uk>
2025-02-17 22:17:27 +0000

    Added new logo and fixed padding on nav

--- app/page.tsx
+++ app/page.tsx
@@ -77,8 +77,7 @@ export default function Home() {
       <header className="border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-16 items-center justify-between">
           <div className="flex items-center space-x-2">
-            <CodeIcon className="h-6 w-6 text-primary" />
-            <span className="text-xl font-bold">Codetrees</span>
+            <img className="h-6 text-primary" src='logo.png'/>
           </div>
           <div className="flex flex-1 items-center justify-between space-x-4 px-8">
             <div className="relative flex-1 max-w-2xl">
